1e07696d56fe483bc31f475ad2e64b6a1469b2b1
Colemichael16 <colemichael999@gmail.com>
2021-03-23 10:56:05 -0600

    Finished

--- app/src/main/java/org/legendtitans/storyv2/MainActivity4.java
+++ app/src/main/java/org/legendtitans/storyv2/MainActivity4.java
@@ -2,7 +2,9 @@ package org.legendtitans.storyv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity4 extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+    }
+    public void ToMainSix (View v){
+        startActivity(new Intent(getApplicationContext(), MainActivity5.class));
     }
 }
